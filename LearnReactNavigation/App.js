a83b1a1833cd71225b348a3252918573b5cd89da
DESKTOP-7CLU8AB\dev3_4 <uhas2003@naver.com>
2022-11-04 17:27:17 +0900

    rn

---
+++ LearnReactNavigation/App.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from './screesns/HomeScreen';
+import DetailScreen from './screesns/DetailScreen';
+import { Text, TouchableOpacity, View } from 'react-native';
+import HeaderlessScreen from './screesns/HeaderlessScreen';
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: '홈',
+            headerStyle: {
+              backgroundColor: '#29b6f6',
+            },
+            headerTintColor: '#ffffff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 20,
+            },
+          }}
+        />
+
+        <Stack.Screen
+          name="Detail"
+          component={DetailScreen}
+          options={{
+            headerBackVisible: false,
+            headerLeft: ({ onPress }) => (
+              <TouchableOpacity onPress={onPress}>
+                <Text>Left</Text>
+              </TouchableOpacity>
+            ),
+            headerTitle: ({ children }) => (
+              <View>
+                <Text>{children}</Text>
+              </View>
+            ),
+            headerRight: () => (
+              <View>
+                <Text>Right</Text>
+              </View>
+            )
+          }}
+        />
+
+        <Stack.Screen
+          name="Headerless"
+          component={HeaderlessScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+
+    </NavigationContainer>
+  )
+}
+
+export default App;
